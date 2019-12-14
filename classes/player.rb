03d255221544ea08b34c5f93927a5fdf645d38b8
jamespjbennett <jamespjbennett@hotmail.com>
2019-12-14 22:59:25 +0000

    passing test for player and position creation

--- classes/player.rb
+++ classes/player.rb
@@ -1,17 +1,21 @@
 class Player
 
+  attr_accessor :name, :position, :attack, :defence
+
   def initialize (name, position, attack, defence)
     @name = name
     @position = position
     @attack = attack
     @defence = defence
   end
-  
+
 end
 
 
 class Position
 
+  attr_accessor :name
+  
   def initialize (name)
     @name = name
   end
