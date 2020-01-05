8ef03fb659de756d48501776b3b2a156c6b71d25
jamespjbennett <jamespjbennett@hotmail.com>
2020-01-05 14:00:58 +0000

    score formatter test passing

--- lib/fixture.rb
+++ lib/fixture.rb
@@ -28,8 +28,18 @@ end
 
 class ScoreFormatter
 
-  def call(home_team_goals, away_team_goals)
+  def initialize(home_team, away_team)
+    @home_team = home_team
+    @away_team = away_team
+  end
 
+  def call(home_team_goals, away_team_goals)
+    p team_goal_string(home_team_goals,@home_team) + " - " + team_goal_string(away_team_goals,@away_team)
+  end
+
+  def team_goal_string(team_goals, team)
+    goal_string = "#{team.name} #{team_goals.length} "
+    goal_string += "( #{team_goals.map{|goal| goal.scorer.name }.join(", ")} ) "
   end
 
 end
--- spec/shared/fixtures.rb
+++ spec/shared/fixtures.rb
@@ -9,11 +9,11 @@ RSpec.shared_context "fixtures" do
   include_context "teams"
 
   let(:manu_liverpool){
-    Fixture.new(team_man_united,  team_liverpool, ScoreFormatter.new)
+    Fixture.new(team_man_united,  team_liverpool, ScoreFormatter.new(team_man_united, team_liverpool))
   }
 
   let(:watford_liverpool){
-    Fixture.new(team_watford,  team_liverpool, ScoreFormatter.new)
+    Fixture.new(team_watford,  team_liverpool, ScoreFormatter.new(team_man_united, team_liverpool))
   }
 
 end
